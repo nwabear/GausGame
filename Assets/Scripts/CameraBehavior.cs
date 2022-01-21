70a010b9b04a4c352520b2c539ec1285b868a5f1
Bear Daniel <iambeardaniel@gmail.com>
2022-01-21 09:42:34 -0600

    add scrolling background

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -12,11 +12,12 @@ public class CameraBehavior : MonoBehaviour
 
     public GameObject player;
     public GameObject camera;
+    public float scroll_speed;
     // Update is called once per frame
     void Update()
     {
         if(player.transform.position.y >= 1.5) {
-            camera.transform.position = new Vector3(camera.transform.position.x, player.transform.position.y, camera.transform.position.z);
+            camera.transform.position = new Vector3(camera.transform.position.x, ((player.transform.position.y) * scroll_speed), camera.transform.position.z);
         }
     }
 }
